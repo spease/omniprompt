47dccda976a82ebb7fd58674efd68bbe7d958314
Steven Pease <peasteven@gmail.com>
2022-04-22 08:39:54 -0700

    Performance improvements

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use bytesize::ByteSize;
-use chrono::Local;
 use anyhow::{Context, Result, anyhow};
+use chrono::Local;
+use core::str::FromStr;
 #[cfg(feature="git")]
 use git2::Repository;
-use std::ffi::{OsStr, OsString};
 use std::io::Write;
 use std::net::IpAddr;
 use std::path::Path;
-use std::str::FromStr;
-use sysinfo::{NetworkExt, ProcessExt, RefreshKind, SystemExt};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, StandardStreamLock, WriteColor};
 
 struct ColorCache {
@@ -55,13 +52,13 @@ impl Default for ColorCache {
     }
 }
 
-struct FieldWriter {
+struct FieldWriter<'a> {
     color_cache: ColorCache,
     column_count: usize,
     errors: String,
-    exit_code: OsString,
+    exit_code: Option<i32>,
     row_count: usize,
-    stream: StandardStream,
+    stream: StandardStreamLock<'a>,
 }
 
 #[derive(Copy, Clone, Eq, PartialEq)]
@@ -69,26 +66,28 @@ enum Field {
     ExitCode,
     #[cfg(feature="git")]
     Git,
+    #[cfg(feature="network")]
     Network,
+    #[cfg(feature="platform")]
     Platform,
     Ppid,
     Prompt,
     Pwd,
     Time,
-    #[cfg(unix)]
+    #[cfg(feature="tty")]
     Tty,
     Whoami,
 }
 
-impl Drop for FieldWriter {
+impl<'a> Drop for FieldWriter<'a> {
     fn drop(&mut self) {
         self.stream.reset().expect("Failed to reset output stream");
     }
 }
 
-impl FieldWriter {
-    fn new(stream: StandardStream, exit_code: OsString) -> Self {
-        FieldWriter {
+impl<'a> FieldWriter<'a> {
+    fn new(stream: StandardStreamLock<'a>, exit_code: Option<i32>) -> Self {
+        Self {
             color_cache: ColorCache::default(),
             column_count: 0,
             errors: String::new(),
@@ -105,11 +104,26 @@ impl FieldWriter {
         Ok(())
     }
 
-    fn print_field(function: Field, color_cache: &ColorCache, exit_code: &OsStr, stream: &mut StandardStreamLock) -> Result<()> {
+    fn print_field(function: Field, color_cache: &ColorCache, exit_code: Option<i32>, stream: &mut StandardStreamLock<'a>) -> Result<()> {
+        #[cfg(any(feature="network",not(unix)))]
+        let si = {
+            use sysinfo::{RefreshKind, SystemExt};
+            let mut rk = RefreshKind::new();
+            #[cfg(feature="network")]
+            {
+                rk = rk.with_networks();
+            }
+            #[cfg(not(unix))]
+            {
+                use sysinfo::ProcessRefreshKind;
+                rk = rk.with_processes(ProcessRefreshKind::new());
+            }
+            sysinfo::System::new_with_specifics(rk)
+        };
         match function {
             Field::ExitCode => {
-                stream.set_color(if exit_code == "0" { &color_cache.green_bold } else { &color_cache.red_bold})?;
-                stream.write_all(exit_code.to_str().ok_or_else(||anyhow!("Unable to convert exit_code to string"))?.as_bytes())?;
+                stream.set_color(if exit_code == Some(0) { &color_cache.green_bold } else { &color_cache.red_bold})?;
+                stream.write_all(exit_code.map(|s|s.to_string()).unwrap_or(String::new()).as_bytes())?;
             }
             #[cfg(feature="git")]
             Field::Git => {
@@ -118,33 +132,40 @@ impl FieldWriter {
                     stream.write_all(repo.head().context("trying to get HEAD")?.shorthand().unwrap_or("<UNKNOWN>").as_bytes())?;
                 }
             },
+            #[cfg(feature="network")]
             Field::Network => {
-                let si = sysinfo::System::new_with_specifics(RefreshKind::new().with_networks());
+                use sysinfo::NetworkExt;
+                use bytesize::ByteSize;
                 let (upload, download) = si.networks().into_iter().map(|(_, nw)| (ByteSize(nw.received()), ByteSize(nw.transmitted()))).fold((ByteSize(0),ByteSize(0)), |sum,current|(sum.0+current.0, sum.1+current.1));
                 write!(stream, "↑{}↓{}", upload, download)?;
             },
+            #[cfg(feature="platform")]
             Field::Platform => {
                 stream.set_color(&color_cache.red)?;
-                let arch = target_info::Target::arch();
-                let oi = os_info::get();
-                #[cfg(unix)]
-                write!(
-                    stream,
-                    "{} ({})/{}/{}",
-                    oi.os_type(),
-                    oi.version(),
-                    nix::sys::utsname::uname().release(),
-                    arch
-                )?;
-                #[cfg(not(unix))]
-                write!(stream, "{} ({})/{}", oi.os_type(), oi.version(), arch)?;
+                 let oi = os_info::get();
+                 #[cfg(unix)]
+                 write!(
+                     stream,
+                     "{} ({})/{}/{}",
+                     oi.os_type(),
+                     oi.version(),
+                     nix::sys::utsname::uname()?.release().to_string_lossy(),
+                     std::env::consts::ARCH
+                 )?;
+                 #[cfg(not(unix))]
+                 write!(stream, "{} ({})/{}", oi.os_type(), oi.version(), std::env::consts::ARCH)?;
             },
             Field::Ppid => {
                 stream.set_color(&color_cache.yellow)?;
-                let si = sysinfo::System::new_with_specifics(RefreshKind::new().with_processes());
-                let pid = sysinfo::get_current_pid().map_err(|e|anyhow!("{}",e))?;
-                let parent_pid = si.process(pid).ok_or_else(||anyhow!("Couldn't find current PID"))?.parent().ok_or_else(||anyhow!("No parent for current process"))?;
-                write!(stream, "{}", parent_pid)?;
+                #[cfg(unix)]
+                write!(stream, "{}", std::os::unix::process::parent_id())?;
+                #[cfg(not(unix))]
+                {
+                    use sysinfo::{ProcessExt, SystemExt};
+                    let pid = sysinfo::get_current_pid().map_err(|e|anyhow!("{}",e))?;
+                    let parent_pid = si.process(pid).ok_or_else(||anyhow!("Couldn't find current PID"))?.parent().ok_or_else(||anyhow!("No parent for current process"))?;
+                    write!(stream, "{}", parent_pid)?;
+                }
             }
             Field::Prompt => {
                 stream.set_color(&color_cache.magenta_bold)?;
@@ -168,13 +189,13 @@ impl FieldWriter {
                 // stream.write_all(Local::now().to_rfc3339().as_bytes())?;
                 write!(stream, "{}", Local::now().format("%Y-%m-%d %H:%M:%S%.3f %Z"))?;
             }
-            #[cfg(unix)]
+            #[cfg(feature="tty")]
             Field::Tty => {
                 use std::os::unix::io::AsRawFd;
                 use std::os::unix::ffi::OsStrExt;
                 stream.set_color(&color_cache.yellow)?;
                 let stdin_fd = std::io::stdin().as_raw_fd();
-                stream.write_all(nix::unistd::ttyname(stdin_fd)?.into_os_string().as_os_str().as_bytes())?;
+                stream.write_all(nix::unistd::ttyname(stdin_fd)?.as_str_lossy())?;
             }
             Field::Whoami => {
                 stream.set_color(&color_cache.cyan_bold)?;
@@ -198,16 +219,14 @@ impl FieldWriter {
     }
 
     fn print_section(&mut self, function: Field) -> Result<()> {
-        let mut stream = self.stream.lock();
-
         if self.column_count != 0 {
-            stream.reset()?;
-            stream.write_all(if self.row_count == 0 { b" - " } else { b"-" })?;
+            self.stream.reset()?;
+            self.stream.write_all(if self.row_count == 0 { b" - " } else { b"-" })?;
         }
-        stream.set_color(&self.color_cache.blue_bold)?;
-        stream.write_all(if self.column_count != 0 { b"[" } else if self.row_count == 0 { "┌─[".as_bytes() } else { "└─[".as_bytes() })?;
+        self.stream.set_color(&self.color_cache.blue_bold)?;
+        self.stream.write_all(if self.column_count != 0 { b"[" } else if self.row_count == 0 { "┌─[".as_bytes() } else { "└─[".as_bytes() })?;
 
-        if let Err(e) = Self::print_field(function, &self.color_cache, &self.exit_code, &mut stream) {
+        if let Err(e) = Self::print_field(function, &self.color_cache, self.exit_code, &mut self.stream) {
             use std::fmt::Write;
             if self.errors.is_empty() {
                 write!(self.errors, "{:?}", e)?;
@@ -217,16 +236,15 @@ impl FieldWriter {
         }
         self.column_count += 1;
 
-        stream.set_color(&self.color_cache.blue_bold)?;
-        stream.write_all(if function != Field::Prompt { b"]" } else { b"]> " })?;
+        self.stream.set_color(&self.color_cache.blue_bold)?;
+        self.stream.write_all(if function != Field::Prompt { b"]" } else { b"]> " })?;
 
         Ok(())
     }
 
     fn print_errors(&mut self) -> Result<()> {
-        let mut stream = self.stream.lock();
-        stream.set_color(&self.color_cache.red_bold)?;
-        stream.write_all(self.errors.as_bytes())?;
+        self.stream.set_color(&self.color_cache.red_bold)?;
+        self.stream.write_all(self.errors.as_bytes())?;
         Ok(())
     }
 
@@ -235,14 +253,15 @@ impl FieldWriter {
     }
 }
 
-fn print_default(rval: impl Into<OsString>) -> Result<()> {
-    let mut fw = FieldWriter::new(StandardStream::stdout(ColorChoice::Always), rval.into());
+fn print_default(exit_code: Option<i32>) -> Result<()> {
+    let stdout = StandardStream::stdout(ColorChoice::Always);
+    let mut fw = FieldWriter::new(stdout.lock(), exit_code);
 
     fw.print_section(Field::Whoami)?;
     fw.print_section(Field::Pwd)?;
-    #[cfg(unix)]
     fw.print_section(Field::Ppid)?;
     fw.print_section(Field::Time)?;
+    #[cfg(feature="platform")]
     fw.print_section(Field::Platform)?;
     fw.print_line()?;
     fw.print_section(Field::ExitCode)?;
@@ -258,9 +277,7 @@ fn print_default(rval: impl Into<OsString>) -> Result<()> {
 }
 
 fn main() -> Result<()> {
-    let rval = std::env::args_os()
-        .nth(1)
-        .unwrap_or_default();
+    let rval = std::env::args_os().nth(1).filter(|s|!s.is_empty()).map(|s|i32::from_str(&s.to_string_lossy())).transpose()?;
     print_default(rval)
 }
 
@@ -286,13 +303,15 @@ mod test {
     test!(exit_code, Field::ExitCode);
     #[cfg(feature="git")]
     test!(git, Field::Git);
+    #[cfg(feature="network")]
     test!(network, Field::Network);
+    #[cfg(feature="platform")]
     test!(platform, Field::Platform);
     test!(ppid, Field::Ppid);
     test!(prompt, Field::Prompt);
     test!(pwd, Field::Pwd);
     test!(time, Field::Time);
-    #[cfg(unix)]
+    #[cfg(feature="tty")]
     test!(tty, Field::Tty);
     test!(whoami, Field::Whoami);
 
